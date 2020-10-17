18116a32212ed37dca8d56e0729ee63b10f70d42
DURVESH.PAL <63143142+Durveshpal@users.noreply.github.com>
2020-10-17 13:04:31 +0530

    Update swapcase.py
    hey, I change in your code if you like it then plz merge it

--- swapcase.py
+++ swapcase.py
@@ -1,8 +1,16 @@
 def swap_case(s):
-    result = ""
-    for letter in s:
-        if letter == letter.upper():
-            result += letter.lower()
-        else:
-            result += letter.upper()
-    return result
+    length = len(s)
+    i=0
+    while length:
+        if s1[i].isupper():
+            s[i].lower()
+        elif s[i].islower():
+            s[i].upper()
+        length-=1
+        i+=1
+    return s
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
